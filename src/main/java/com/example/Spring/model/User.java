6e34bdda7391f8d53b2a0a83f8be0ae15acfb3bd
farzadafi <farzadafi40@gmail.com>
2022-09-03 20:20:16 +0430

    Add required fields

--- src/main/java/com/example/Spring/model/User.java
+++ src/main/java/com/example/Spring/model/User.java
@@ -1,4 +1,30 @@
 package com.example.Spring.model;
 
-public class User {
+import com.example.Spring.model.base.BaseEntity;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@Entity
+@Table(name = "Users")
+public class User extends BaseEntity<Integer> {
+
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+
+    @CreationTimestamp
+    private OffsetDateTime creationDate;
+
+    @ManyToOne
+    private Role role;
 }
