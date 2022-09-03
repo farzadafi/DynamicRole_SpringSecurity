efc7916659a34148e1a3f62650b44ab501e55963
farzadafi <farzadafi40@gmail.com>
2022-09-03 20:20:10 +0430

    Add required fields

--- src/main/java/com/example/Spring/model/Url.java
+++ src/main/java/com/example/Spring/model/Url.java
@@ -1,4 +1,21 @@
 package com.example.Spring.model;
 
-public class Url {
+import com.example.Spring.model.base.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@Entity
+public class Url extends BaseEntity<Integer> {
+
+    private String url;
+
+    @ManyToMany(mappedBy = "urls")
+    private Set<Role> role;
 }
