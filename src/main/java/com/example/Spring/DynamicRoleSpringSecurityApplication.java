0ac83d2ef3851ef42fc04f73bf1ba9fcc0ef6a25
farzadafi <farzadafi40@gmail.com>
2022-09-03 20:05:32 +0430

    In the name of God!

---
+++ src/main/java/com/example/Spring/DynamicRoleSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRoleSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicRoleSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring/DynamicRoleSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicRoleSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
