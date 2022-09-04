1b914c36f011240a5de3a36951acd9a964e67a79
farzadafi <farzadafi40@gmail.com>
2022-09-04 15:19:24 +0430

    implement from RoleService

--- src/main/java/com/example/Spring/service/RoleService.java
+++
@@ -1,4 +0,0 @@
-package com.example.Spring.service;
-
-public class RoleService {
-}
---
+++ src/main/java/com/example/Spring/service/role/RoleServiceImpel.java
@@ -0,0 +1,4 @@
+package com.example.Spring.service.role;
+
+public class RoleServiceImpel implements RoleService {
+}
