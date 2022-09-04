b036ea17b90fbbd9a6def3a7822b2c450a10307d
farzadafi <farzadafi40@gmail.com>
2022-09-04 15:23:16 +0430

    add isPermissionRoleToUrl method

--- src/main/java/com/example/Spring/service/role/RoleService.java
+++ src/main/java/com/example/Spring/service/role/RoleService.java
@@ -1,4 +1,7 @@
 package com.example.Spring.service.role;
 
 public interface RoleService {
+
+    boolean isPermissionRoleToUrl(String role, String url);
+
 }
--- src/main/java/com/example/Spring/service/role/RoleServiceImpel.java
+++ src/main/java/com/example/Spring/service/role/RoleServiceImpel.java
@@ -1,4 +1,17 @@
 package com.example.Spring.service.role;
 
+import com.example.Spring.reposiroty.RoleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class RoleServiceImpel implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public boolean isPermissionRoleToUrl(String role, String url){
+        return roleRepository.findRoleWithUrl(role, url).isPresent();
+    }
 }
