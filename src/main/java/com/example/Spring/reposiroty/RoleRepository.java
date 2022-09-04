fcd4a45b63874a556c4a951af1cd6c18e57e5f89
farzadafi <farzadafi40@gmail.com>
2022-09-04 15:22:35 +0430

    add findRoleWithUrl method

--- src/main/java/com/example/Spring/reposiroty/RoleRepository.java
+++ src/main/java/com/example/Spring/reposiroty/RoleRepository.java
@@ -1,4 +1,18 @@
 package com.example.Spring.reposiroty;
 
-public interface RoleRepository {
+import com.example.Spring.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+
+    @Query
+            ("FROM Role AS r join Url AS u ON" +
+                    " r.role = :role and" +
+                    " u.url = :url")
+    Optional<Role> findRoleWithUrl(@Param("role") String role,
+                                   @Param("url") String url);
 }
