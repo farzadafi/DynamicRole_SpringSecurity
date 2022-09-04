86d0b30e29de9059324edd5225d9886dd402ee6e
farzadafi <farzadafi40@gmail.com>
2022-09-04 15:11:26 +0430

    add bCryptPasswordEncoder method for Bean spring

--- src/main/java/com/example/Spring/config/securityConfig.java
+++ src/main/java/com/example/Spring/config/securityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +21,10 @@ public class securityConfig {
                         .anyRequest().permitAll()
                 )
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
