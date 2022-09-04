1d7c7921307011f38064d2561a72113a8b52a55d
farzadafi <farzadafi40@gmail.com>
2022-09-04 15:10:30 +0430

    add mainConfig method for main SecurityConfiguration

--- src/main/java/com/example/Spring/config/securityConfig.java
+++ src/main/java/com/example/Spring/config/securityConfig.java
@@ -1,4 +1,24 @@
 package com.example.Spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true,securedEnabled = true,prePostEnabled = true)
 public class securityConfig {
+
+    @Bean
+    protected SecurityFilterChain mainConfig(HttpSecurity http) throws Exception {
+        return http
+                .antMatcher("/**")
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
 }
