474a8ccad179557d8d2ca2add806f280ee1e0d82
farzadafi <farzadafi40@gmail.com>
2022-09-06 17:37:18 +0430

    add hasPermission method

--- src/main/java/com/example/Spring/service/security/SecurityService.java
+++ src/main/java/com/example/Spring/service/security/SecurityService.java
@@ -1,4 +1,6 @@
 package com.example.Spring.service.security;
 
 public interface SecurityService {
+
+    boolean hasPermission(String role,String url);
 }
--- src/main/java/com/example/Spring/service/security/SecurityServiceImpel.java
+++ src/main/java/com/example/Spring/service/security/SecurityServiceImpel.java
@@ -1,4 +1,17 @@
 package com.example.Spring.service.security;
 
+import com.example.Spring.service.role.RoleServiceImpel;
+
 public class SecurityServiceImpel implements SecurityService {
+
+    private final RoleServiceImpel roleServiceImpel;
+
+    public SecurityServiceImpel(RoleServiceImpel roleServiceImpel) {
+        this.roleServiceImpel = roleServiceImpel;
+    }
+
+    @Override
+    public boolean hasPermission(String role,String url){
+        return roleServiceImpel.isPermissionRoleToUrl(role,url);
+    }
 }
