25c265b951fa15160ad3511478b61ab6ea0eeff6
farzadafi <farzadafi40@gmail.com>
2022-09-06 17:30:28 +0430

    create dynamicRoleTest method

--- src/main/java/com/example/Spring/controller/DynamicRoleController.java
+++ src/main/java/com/example/Spring/controller/DynamicRoleController.java
@@ -1,4 +1,15 @@
 package com.example.Spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dynamicRole")
 public class DynamicRoleController {
+
+    @GetMapping("/test")
+    public String dynamicRoleTest(){
+        return "OK";
+    }
 }
