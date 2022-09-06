3e9d21cde6c0cb4f52ff60fa44361daabfb5ef2a
farzadafi <farzadafi40@gmail.com>
2022-09-06 17:44:17 +0430

    add @PreAuthorize to dynamicRoleTest method

--- src/main/java/com/example/Spring/controller/DynamicRoleController.java
+++ src/main/java/com/example/Spring/controller/DynamicRoleController.java
@@ -1,5 +1,6 @@
 package com.example.Spring.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DynamicRoleController {
 
     @GetMapping("/test")
+    @PreAuthorize("@SecurityServiceImpel.hasPermission(@SecurityServiceImpel.currentUser.getRole().role,'/dynamicRole/test')")
     public String dynamicRoleTest(){
         return "OK";
     }
