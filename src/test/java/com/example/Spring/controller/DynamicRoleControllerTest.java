ca92bfd2cdf72ad76255a688908fa32f90b44dbc
farzadafi <farzadafi40@gmail.com>
2022-09-17 19:04:57 +0430

    add setup method

--- src/test/java/com/example/Spring/controller/DynamicRoleControllerTest.java
+++ src/test/java/com/example/Spring/controller/DynamicRoleControllerTest.java
@@ -1,7 +1,49 @@
 package com.example.Spring.controller;
 
+import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DynamicRoleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+
+    @BeforeAll
+    static void setup(@Autowired DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("dynamicRoleTestData.sql"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
