1e2c25d9e9fcd0f2828fe96378a229b51351ead3
farzadafi <farzadafi40@gmail.com>
2022-09-17 19:37:42 +0430

    add dynamicRoleTest method

--- src/test/java/com/example/Spring/controller/DynamicRoleControllerTest.java
+++ src/test/java/com/example/Spring/controller/DynamicRoleControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.Spring.controller;
 
+import com.example.Spring.model.Role;
+import com.example.Spring.model.User;
 import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,7 +36,15 @@ class DynamicRoleControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    Role role = Role.builder()
+            .role("ROLE_ADMIN").build();
 
+    User user = User.builder()
+            .firstname("farzad")
+            .lastname("afshar")
+            .username("farzadAfi")
+            .password("afi")
+            .role(role).build();
 
 
     @BeforeAll
@@ -45,5 +56,15 @@ class DynamicRoleControllerTest {
         }
     }
 
+    @Test
+    public void dynamicRoleTest() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/dynamicRole/test")
+                .with(SecurityMockMvcRequestPostProcessors.user(user))
+                .contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
 
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
 }
