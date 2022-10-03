775fbf8c473449c9a27a61736c888c10d28347bb
farzadafi <farzadafi40@gmail.com>
2022-10-03 21:24:27 +0330

    fix PreAuthorize and add HttpServletRequest to method

--- src/main/java/com/example/Spring/controller/DynamicRoleController.java
+++ src/main/java/com/example/Spring/controller/DynamicRoleController.java
@@ -4,14 +4,15 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/dynamicRole")
 public class DynamicRoleController {
 
     @GetMapping("/test")
-    @PreAuthorize("@SecurityServiceImpel.isAuthenticated(@SecurityServiceImpel.currentUser.getRole().role,'/dynamicRole/test')")
-    public String dynamicRoleTest(){
+    @PreAuthorize("@SecurityServiceImpel.isAuthenticated(@SecurityServiceImpel.currentUser.getRole().role,#request.requestURI)")
+    public String dynamicRoleTest(HttpServletRequest request){
         return "OK";
     }
 }
